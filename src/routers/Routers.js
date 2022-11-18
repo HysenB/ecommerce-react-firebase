568e6c1d29ddf65e7b1d0972a2a1032b3fe0b0b8
HysenB <hysi2000@live.com>
2022-11-18 16:58:13 +0100

    routing

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom";
+
+// Pages
+import Home from "../pages/Home"
+import Shop from "../pages/Shop"
+import Cart from "../pages/Cart"
+import ProductDetails from "../pages/ProductDetails"
+import Checkout from "../pages/Checkout"
+import Login from "../pages/Login"
+import Signup from "../pages/Signup"
 
 const Router = () => {
-    return (
-        <div>Router</div>
-    )
+    return <Routes>
+        <Route path='home' element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='shop/:id' element={<ProductDetails />} />
+        <Route path='cart' element={<Cart />} />
+        <Route path='checkout' element={<Checkout />} />
+        <Route path='login' element={<Login />} />
+        <Route path='signup' element={<Signup />} />
+    </Routes>
 }
 
 export default Router
