06e3cd5e89abeafdd594055dccc54860528f6fd2
HysenB <hysi2000@live.com>
2022-11-20 19:12:33 +0100

    trending products & best sales

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import products from '../assets/data/products'
 
 import Helmet from '../components/helmet/Helmet'
 import "../styles/home.css";
@@ -10,10 +11,23 @@ import { Container, Row, Col } from 'reactstrap';
 import heroImg from '../assets/images/hero-img.png'
 
 import Services from '../services/Services';
+import ProductsList from '../components/UI/ProductsList';
 
 const Home = () => {
 
+    const [trendingProducts, setTrendingProducts] = useState([]);
+    const [bestSalesProducts, setBestSalesPrOducts] = useState([]);
+
     const year = new Date().getFullYear();
+
+    useEffect(() => {
+        const filteredTrendingProducts = products.filter((item) => item.category === 'chair');
+
+        const filteredBestSalesProducts = products.filter((item) => item.category === 'sofa');
+
+        setTrendingProducts(filteredTrendingProducts);
+        setBestSalesPrOducts(filteredBestSalesProducts);
+    }, [])
 
     return <Helmet title={'Home'}>
         <section className="hero__section">
@@ -48,6 +62,18 @@ const Home = () => {
                     <Col lg='12' className='text-center'>
                         <h2 className="section__title">Trending Products</h2>
                     </Col>
+                    <ProductsList data={trendingProducts} />
+                </Row>
+            </Container>
+        </section>
+
+        <section className="best__sales">
+            <Container>
+                <Row>
+                    <Col lg='12' className='text-center'>
+                        <h2 className="section__title">Best Sales</h2>
+                    </Col>
+                    <ProductsList data={bestSalesProducts} />
                 </Row>
             </Container>
         </section>
