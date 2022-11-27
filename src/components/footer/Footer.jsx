0c3b6f457b6d09051325729e19cb772fdde49355
HysenB <hysi2000@live.com>
2022-11-27 21:10:39 +0100

    mobile responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
     return <footer className="footer">
         <Container>
             <Row>
-                <Col lg='4'>
+                <Col lg='4' className='mb-4' md='6'>
                     <div className="logo">
 
                         <div>
@@ -22,7 +22,7 @@ const Footer = () => {
                         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit veritatis perferendis enim doloremque tenetur eveniet consequatur commodi et non nobis.
                     </p>
                 </Col>
-                <Col lg='3'>
+                <Col lg='3' md='3' className='mb-4'>
                     <div className="footer__quick-links">
                         <h4 className="quick__links-title">Top Categories</h4>
                         <ListGroup className='mb-3'>
@@ -45,7 +45,7 @@ const Footer = () => {
                         </ListGroup>
                     </div>
                 </Col>
-                <Col lg='2'>
+                <Col lg='2' md='3' className='mb-4'>
                     <div className="footer__quick-links">
                         <h4 className="quick__links-title">Useful Links</h4>
                         <ListGroup className='mb-3'>
@@ -68,7 +68,7 @@ const Footer = () => {
                         </ListGroup>
                     </div>
                 </Col>
-                <Col lg='3'>
+                <Col lg='3' md='4'>
                     <div className="footer__quick-links">
                         <h4 className="quick__links-title">Contact</h4>
                         <ListGroup className='footer__contact'>
